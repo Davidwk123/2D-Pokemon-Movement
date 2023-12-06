7b3e54959009a76eeec7ef4ee643207dddeaca55
Kalin <kalinkriley12@gmail.com>
2023-12-06 08:56:50 -0800

    got basic movement to work

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,28 @@
 extends KinematicBody2D
 const SPEED = 100.0
 
+onready var anim = get_node("AnimatedSprite")
+
+func _ready():
+	anim.play("idle")
+	
+
 func _physics_process(delta):
 	var input = Vector2()
-	input.x += float(Input.is_action_pressed('ui_right'))
-	input.x -= float(Input.is_action_pressed('ui_left'))
-	input.y -= float(Input.is_action_pressed('ui_up'))
-	input.y += float(Input.is_action_pressed('ui_down'))
-
+	if(Input.is_action_pressed('ui_right')):
+		input.x += float(Input.is_action_pressed('ui_right'))
+		anim.play("right")
+	if(Input.is_action_pressed('ui_left')):
+		anim.play("left")
+		input.x -= float(Input.is_action_pressed('ui_left'))
+	if(Input.is_action_pressed('ui_up')):
+		anim.play("up")
+		input.y -= float(Input.is_action_pressed('ui_up'))
+	if (Input.is_action_pressed('ui_down')):
+		anim.play("down")
+		input.y += float(Input.is_action_pressed('ui_down'))
+	
+	
 	if input.length() != 0:
 		input = input.normalized()
 
